c8bc63ae064ac15e936348e52338b3e11d6f89fe
Nazarii Semeniuk <qnazar.semenyuk@gmail.com>
2023-06-13 15:50:52 +0300

    Initial commit

---
+++ src/balance-sheet.webcomponent.js
@@ -0,0 +1,223 @@
+import GhHtmlElement from "@gudhub/gh-html-element";
+import html from "./balance-sheet.html";
+import './style.scss';
+
+import Handsontable from "handsontable";
+import HyperFormula from 'hyperformula';
+import "handsontable/dist/handsontable.full.css";
+
+const hyperformulaInstance = HyperFormula.buildEmpty({
+
+});
+
+class GhBalanceSheet extends GhHtmlElement {
+
+    // Constructor with super() is required for native web component initialization
+
+    constructor() {
+        super();
+        this.buttonListenerActivated = false;
+    }
+
+    // onInit() is called after parent gh-element scope is ready
+
+    async onInit() {
+        super.render(html);
+
+        const cont = angular.element(this.querySelector('.range'));
+
+        const decorator = {
+            data_type: 'date',
+            field_name: 'Період',
+            data_model: {
+                data_range: true
+            }
+        }
+
+        const element = angular.element(`<gh-element decorator='${JSON.stringify(decorator)}' value="value"></gh-element>`);
+        const compiler = gudhub.ghconstructor.angularInjector.get('$compile');
+        const compiled = compiler(element);
+
+        cont.append(element);
+
+        compiled(this.scope);
+
+        if(!this.value) {
+            this.value = `${gudhub.util.getDate('month_past,past') + 86400000}:${gudhub.util.getDate('month_current,current')}`
+        }
+
+        this.renderTable();
+    }
+
+    // onUpdate() is called after value was updated
+
+    onUpdate() {
+        if(this.value && !Array.isArray(this.value)) {
+            this.renderTable();
+        }
+    }
+
+    async renderTable() {
+        const { app_id, count_field, credit_account_field, credit_subaccount_field, debit_account_field, debit_subaccount_field, date_field, summ_field } = this.scope.field_model.data_model;
+
+        if(!app_id || !count_field || !credit_account_field || !debit_account_field || !date_field || !summ_field) {
+            return;
+        }
+
+        const items = await gudhub.getItems(app_id);
+
+        const result = {};
+        const splitRange = this.value.split(':');
+        const rangeStart = Number(splitRange[0]);
+        const rangeEnd = Number(splitRange[1]);
+
+        for (const item of items) {
+            const debitAccount = await gudhub.getInterpretationById(app_id, item.item_id, debit_account_field, 'value');
+            const creditAccount = await gudhub.getInterpretationById(app_id, item.item_id, credit_account_field, 'value');
+
+            if (!result[debitAccount]) {
+                result[debitAccount] = {
+                    past: [],
+                    current: [],
+                    future: []
+                };
+            }
+
+            if (!result[creditAccount]) {
+                result[creditAccount] = {
+                    past: [],
+                    current: [],
+                    future: []
+                }
+            }
+
+            const transaction = {
+                summ: item.fields.find(field => field.field_id == summ_field)?.field_value,
+                count: item.fields.find(field => field.field_id == count_field)?.field_value
+            }
+
+            const date = Number(item.fields.find(field => field.field_id == date_field)?.field_value);
+
+            if (rangeStart < date && date < rangeEnd) {
+                result[debitAccount].current.push({ type: 'debit', ...transaction });
+                result[creditAccount].current.push({ type: 'credit', ...transaction });
+            } else if (date < rangeStart) {
+                result[debitAccount].past.push({ type: 'debit', ...transaction });
+                result[creditAccount].past.push({ type: 'credit', ...transaction });
+            } else if (date > rangeEnd) {
+                result[debitAccount].future.push({ type: 'debit', ...transaction });
+                result[creditAccount].future.push({ type: 'credit', ...transaction });
+            }
+        }
+
+        const container = this.querySelector('.balance-sheet');
+
+        const data = [
+            ["Рахунок", "Сальдо на початок періоду", '', "Обороти за період", '', "Сальдо на кінець періоду", ''],
+            ['', 'Дебет', 'Кредит', 'Дебет', 'Кредим', 'Дебет', 'Кредит']
+        ];
+
+        for (const index in result) {
+            const arr = [
+                index,
+                this.sumOperations(result[index].past, 'debit'),
+                this.sumOperations(result[index].past, 'credit'),
+                this.sumOperations(result[index].current, 'debit'),
+                this.sumOperations(result[index].current, 'credit'),
+                this.sumOperations(result[index].future, 'debit'),
+                this.sumOperations(result[index].future, 'credit')
+            ];
+            data.push(arr);
+        }
+
+        data.push(['Разом', `=SUM(B3:B${data.length})`, `=SUM(C3:C${data.length})`, `=SUM(D3:D${data.length})`, `=SUM(E3:E${data.length})`, `=SUM(F3:F${data.length})`, `=SUM(G3:G${data.length})`])
+
+        const table = new Handsontable(container, {
+            licenseKey: 'non-commercial-and-evaluation',
+            data: data,
+            rowHeaders: false,
+            colHeaders: false,
+            colWidths: 150,
+            mergeCells: [
+                { row: 0, col: 1, rowspan: 1, colspan: 2 },
+                { row: 0, col: 3, rowspan: 1, colspan: 2 },
+                { row: 0, col: 5, rowspan: 1, colspan: 2 },
+                { row: 0, col: 0, rowspan: 2, colspan: 1 },
+            ],
+            formulas: {
+                engine: hyperformulaInstance
+            },
+            cells(row, col) {
+                const cellProperties = {};
+
+                if (row === 0 || row === 1) {
+                    cellProperties.renderer = function (instance, td, row, col, prop, value, cellProperties) {
+                        Handsontable.renderers.TextRenderer.apply(this, arguments);
+                        td.style.fontWeight = 'bold';
+                        td.style.background = 'rgb(204, 238, 204)';
+                        td.style.color = 'green';
+                        td.style.textAlign = 'center';
+                    }
+                }
+
+                if(col === 0 && row !== 0 && row !== 1) {
+                    cellProperties.renderer = function (instance, td, row, col, prop, value, cellProperties) {
+                        Handsontable.renderers.TextRenderer.apply(this, arguments);
+                        td.style.fontWeight = 'bold';
+                        td.style.textAlign = 'center';
+                    }
+                }
+
+                if (row === data.length - 1 && col !== 0) {
+                    cellProperties.renderer = function (instance, td, row, col, prop, value, cellProperties) {
+                        Handsontable.renderers.TextRenderer.apply(this, arguments);
+                        td.style.fontWeight = 'bold';
+                    }
+                }
+
+                return cellProperties;
+            }
+        });
+
+        if(!this.buttonListenerActivated) {
+
+            const exportPlugin = table.getPlugin('exportFile');
+
+            const button = this.querySelector('button');
+
+            button.addEventListener('click', () => {
+                exportPlugin.downloadFile('csv', {
+                    bom: false,
+                    columnDelimiter: ',',
+                    columnHeaders: false,
+                    exportHiddenColumns: true,
+                    exportHiddenRows: true,
+                    fileExtension: 'csv',
+                    filename: 'Handsontable-CSV-file_[YYYY]-[MM]-[DD]',
+                    mimeType: 'text/csv',
+                    rowDelimiter: '\r\n',
+                    rowHeaders: true
+                });
+            });
+        
+            this.buttonListenerActivated = true;
+
+        }
+    }
+
+    sumOperations(operations, type) {
+        return operations.reduce((acc, item) => {
+            if (item.type === type) {
+                return acc + Number(item.summ);
+            }
+            return acc;
+        }, 0);
+    }
+
+}
+
+// Register web component only if it is not registered yet
+
+if (!customElements.get('gh-balance-sheet')) {
+    customElements.define('gh-balance-sheet', GhBalanceSheet);
+}
---
+++ src/entry.js
@@ -0,0 +1,233 @@
+import './balance-sheet.webcomponent.js';
+
+export default class GhInputData {
+
+    /*------------------------------- FIELD TEMPLATE --------------------------------------*/
+
+    getTemplate() {
+        return {
+            constructor: 'field',
+            name: 'Balance Sheet',
+            icon: 'table',
+            model: {
+                field_id: 0,
+                field_name: 'Balance Sheet',
+                field_value: '',
+                data_type: 'balance_sheet',
+                data_model: {
+                    interpretation: [{
+                        src: 'form',
+                        id: 'default',
+                        settings: {
+                            editable: 1,
+                            show_field_name: 1,
+                            show_field: 1
+                        },
+                        style: { position: "beetwen" }
+                    }]
+                }
+            }
+        };
+    }
+
+    /*------------------------------- INTERPRETATION --------------------------------------*/
+
+    getInterpretation(gudhub, value, appId, itemId, field_model) {
+
+        return [{
+            id: 'default',
+            name: 'Default',
+            content: () =>
+                '<gh-balance-sheet app-id="{{appId}}" item-id="{{itemId}}" field-id="{{fieldId}}"></gh-balance-sheet>'
+        }, {
+            id: 'value',
+            name: 'Value',
+            content: () => value
+        }];
+    }
+
+    /*--------------------------  SETTINGS --------------------------------*/
+
+    getSettings(scope) {
+        return [{
+            title: 'Options',
+            type: 'general_setting',
+            icon: 'menu',
+            columns_list: [
+                [
+                    {
+                        type: 'ghElement',
+                        property: 'data_model.app_id',
+                        data_model: function () {
+                            return {
+                                data_type: 'app',
+                                field_name: 'App',
+                                name_space: 'app',
+                                data_model: {
+                                    current_app: false,
+                                    interpretation: [{
+                                        src: 'form',
+                                        id: 'with_text',
+                                        settings: {
+                                            editable: 1,
+                                            show_field_name: 1,
+                                            show_field: 1
+                                        },
+                                    }]
+                                }
+                            }
+                        }
+                    },
+                    {
+                        type: 'ghElement',
+                        property: 'data_model.debit_account_field',
+                        data_model: function (fieldModel) {
+                            return {
+                                data_type: 'field',
+                                field_name: 'Debit account field',
+                                name_space: 'debit_account_field',
+                                data_model: {
+                                    app_id: fieldModel.data_model.app_id
+                                }
+                            }
+                        },
+                        onInit: function(settingScope, fieldModel) {
+                            settingScope.$watch(function() {
+                                return fieldModel.data_model.app_id;
+                            }, function(newValue) {
+                                settingScope.field_model.data_model.app_id = newValue;
+                            });
+                        }
+                    },
+                    {
+                        type: 'ghElement',
+                        property: 'data_model.credit_account_field',
+                        data_model: function (fieldModel) {
+                            return {
+                                data_type: 'field',
+                                field_name: 'Credit account field',
+                                name_space: 'credit_account_field',
+                                data_model: {
+                                    app_id: fieldModel.data_model.app_id
+                                }
+                            }
+                        },
+                        onInit: function(settingScope, fieldModel) {
+                            settingScope.$watch(function() {
+                                return fieldModel.data_model.app_id;
+                            }, function(newValue) {
+                                settingScope.field_model.data_model.app_id = newValue;
+                            });
+                        }
+                    },
+                    {
+                        type: 'ghElement',
+                        property: 'data_model.debit_subaccount_field',
+                        data_model: function (fieldModel) {
+                            return {
+                                data_type: 'field',
+                                field_name: 'Debit subaccount field',
+                                name_space: 'debit_subaccount_field',
+                                data_model: {
+                                    app_id: fieldModel.data_model.app_id
+                                }
+                            }
+                        },
+                        onInit: function(settingScope, fieldModel) {
+                            settingScope.$watch(function() {
+                                return fieldModel.data_model.app_id;
+                            }, function(newValue) {
+                                settingScope.field_model.data_model.app_id = newValue;
+                            });
+                        }
+                    },
+                    {
+                        type: 'ghElement',
+                        property: 'data_model.credit_subaccount_field',
+                        data_model: function (fieldModel) {
+                            return {
+                                data_type: 'field',
+                                field_name: 'Credit subaccount field',
+                                name_space: 'credit_subaccount_field',
+                                data_model: {
+                                    app_id: fieldModel.data_model.app_id
+                                }
+                            }
+                        },
+                        onInit: function(settingScope, fieldModel) {
+                            settingScope.$watch(function() {
+                                return fieldModel.data_model.app_id;
+                            }, function(newValue) {
+                                settingScope.field_model.data_model.app_id = newValue;
+                            });
+                        }
+                    },
+                    {
+                        type: 'ghElement',
+                        property: 'data_model.date_field',
+                        data_model: function (fieldModel) {
+                            return {
+                                data_type: 'field',
+                                field_name: 'Date field',
+                                name_space: 'date_field',
+                                data_model: {
+                                    app_id: fieldModel.data_model.app_id
+                                }
+                            }
+                        },
+                        onInit: function(settingScope, fieldModel) {
+                            settingScope.$watch(function() {
+                                return fieldModel.data_model.app_id;
+                            }, function(newValue) {
+                                settingScope.field_model.data_model.app_id = newValue;
+                            });
+                        }
+                    },
+                    {
+                        type: 'ghElement',
+                        property: 'data_model.summ_field',
+                        data_model: function (fieldModel) {
+                            return {
+                                data_type: 'field',
+                                field_name: 'Summ field',
+                                name_space: 'summ_field',
+                                data_model: {
+                                    app_id: fieldModel.data_model.app_id
+                                }
+                            }
+                        },
+                        onInit: function(settingScope, fieldModel) {
+                            settingScope.$watch(function() {
+                                return fieldModel.data_model.app_id;
+                            }, function(newValue) {
+                                settingScope.field_model.data_model.app_id = newValue;
+                            });
+                        }
+                    },
+                    {
+                        type: 'ghElement',
+                        property: 'data_model.count_field',
+                        data_model: function (fieldModel) {
+                            return {
+                                data_type: 'field',
+                                field_name: 'Count field',
+                                name_space: 'count_field',
+
+                                data_model: {
+                                    app_id: fieldModel.data_model.app_id
+                                }
+                            }
+                        },
+                        onInit: function(settingScope, fieldModel) {
+                            settingScope.$watch(function() {
+                                return fieldModel.data_model.app_id;
+                            }, function(newValue) {
+                                settingScope.field_model.data_model.app_id = newValue;
+                            });
+                        }
+                    }
+                ]
+            ]
+        }];
+    }
+}
